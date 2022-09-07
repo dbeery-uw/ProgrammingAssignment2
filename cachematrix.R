03a323f965c5878130ae02340eddf45d9af2311b
dbeery-uw <dbeery24@uw.edu>
2022-09-07 10:32:15 -0700

    Program Assignment #2 (in full)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+#ProgrammingAssignment2
 
-## Write a short comment describing this function
+##Matrix simulation to test functions
+z <- matrix(c(1:4, 10:13, 5:8, 6:9, 5:8, 12:16), nrow = 5, ncol = 5)
+solve(z)
+
+##Function 'makeCacheMatrix' creates a list of functions to:
+  ##(1) set the value of the matrix
+  ##(2) get the value of the matrix
+  ##(3) set the inverse of the matrix
+  ##(4) get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Function 'cacheInverse' first checks to see if the inverse of matrix 'x' was already calculated. 
+## If not, cacheInverse calculates inverse of matrix and caches it. 
+cacheInverse <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
+#Test run
+cacheInverse(makeCacheMatrix(z)) #success!
+
+             
